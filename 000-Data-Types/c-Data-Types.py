332400b57f726dfb9b98194d93d9747fe76054d4
ArnaCode <muslim123helalee@gmail.com>
2025-01-18 19:56:10 +0100

    PY-Data-Types-Added

---
+++ 000-Data-Types/c-Data-Types.py
@@ -0,0 +1,19 @@
+x = 42
+pi = 3.14159
+name = "Matthew"
+is_online = False
+
+numbers = [1, 2, 3, "Py", True]
+dimensions = (1920, 1080)
+
+my_pet = {
+    "name": "George",
+    "breed": "German Sheperd"
+}
+
+unique_numbers = {1 , 2, 3, 3, 4, 4, 4 ,5}
+
+print(unique_numbers)
+
+result = None
+# ----------------------------------------
